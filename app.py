ab9ffb573c9304ec8784599191392753488e13ea
techigh <techigh@amuz.co.kr>
2025-05-19 07:09:45 +0900

    fix: change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return jsonify({"message": "Hello from Flask!"})
+    return jsonify({"message": "Hello from Flask! but using Docker!!! With Github Actions"})
 
 if __name__ == "__main__":
     app.run()
